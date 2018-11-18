be2e4fde63fc0e80be5679220bac12c8308c74ad
TUPOBO <1028461931@qq.com>
2018-11-18 16:01:02 +0800

    add resume website

---
+++ precache-manifest.0a568327494b7e61233ef5d378ac76b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "912559c5fc345dc1b9fa",
+    "url": "/static/js/main.912559c5.chunk.js"
+  },
+  {
+    "revision": "ee35e05727901f0d5a6b",
+    "url": "/static/js/1.ee35e057.chunk.js"
+  },
+  {
+    "revision": "912559c5fc345dc1b9fa",
+    "url": "/static/css/main.9e055d9f.chunk.css"
+  },
+  {
+    "revision": "ee35e05727901f0d5a6b",
+    "url": "/static/css/1.2ecd601f.chunk.css"
+  },
+  {
+    "revision": "432d1fd7b81b4df6e98dbaa13f6aeaef",
+    "url": "/index.html"
+  }
+];
